47a323876255a17a6828da1051a1fc8132ea5680
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-08-03 02:45:56 +0600

    Initial commit

---
+++ Master/src/app/page.jsx
@@ -0,0 +1,198 @@
+"use client";
+import TrackVisibility from "react-on-screen";
+import CountUp from "react-countup";
+import Link from "next/link";
+export default function Home() {
+  const dark = localStorage.getItem("theme");
+  return (
+    <section className=" mt-[60px] ">
+      {/* About Intro */}
+      <div className="rounded-xl bg-white p-[30px] dark:bg-card">
+        <div>
+          <p className="text-[18px] text-text">
+            <span className="dark:text-theme">Hi!</span> There I’m
+          </p>
+          <h2 className="mt-[20px] text-[50px] leading-[60px] text-btn dark:text-white md:text-[62px] md:leading-[72px]">
+            Professional <span className="text-theme">Front-end</span> <br />{" "}
+            Web Developer
+          </h2>
+          <p className="mt-[15px] text-text">
+            I am a Professional Front-end Web Developer and have been working in
+            this field for more than 4 years. We are highly experts in
+            converting PSD href HTML, PSD href Bootstrap, Figma href HTML,
+            Sketch href HTML, and Xd href HTML ,CSS responsive design and
+            pixel-perfect using bootstrap latest version, jQuery, JavaScript. I
+            always take care of code with SEO friendly.
+          </p>
+        </div>
+      </div>
+
+      {/* About US */}
+      <div className="mt-[30px] ">
+        <div className="grid grid-cols-12 gap-[30px]">
+          <div className="col-span-12 md:col-span-5">
+            <div className="rounded-xl bg-white p-[30px] dark:bg-card">
+              <img
+                src={
+                  dark === "dark"
+                    ? "/assets/images/logo.svg"
+                    : "/assets/images/logo-dark.svg"
+                }
+                alt=""
+                className="mt-[10px]"
+              />
+              <div className="mt-[20px]">
+                <p className="text-[14px] text-text dark:text-white">
+                  More Specialized
+                </p>
+                <Link href={"/about"}>
+                  <h3 className="mt-[5px] inline-block text-[26px] tracking-[6px] text-btn dark:text-white">
+                    About Us
+                  </h3>
+                </Link>
+              </div>
+            </div>
+            <div>
+              <div className="mt-[30px] rounded-xl bg-white p-[30px] dark:bg-card">
+                <div className="flex gap-[20px]">
+                  <button className="boxShadow rounded-lg p-[20px] dark:bg-btn dark:shadow-none">
+                    <img src="/assets/images/icon/figma.png" alt="" />
+                  </button>
+                  <button className="boxShadow rounded-lg p-[20px] dark:bg-btn dark:shadow-none ">
+                    <img src="/assets/images/icon/react.png" alt="" />
+                  </button>
+                  <button className="boxShadow rounded-lg p-[20px] dark:bg-btn dark:shadow-none ">
+                    <img src="/assets/images/icon/php.png" alt="" />
+                  </button>
+                </div>
+
+                <div className="mt-[30px]">
+                  <p className="text-[14px] text-text dark:text-white">
+                    Specialization
+                  </p>
+                  <Link href={"/service"}>
+                    <h3 className="mt-[5px] inline-block text-[26px] tracking-[6px] text-btn dark:text-white">
+                      Services Offer{" "}
+                    </h3>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="mt-[30px] rounded-xl bg-white p-[30px] dark:bg-card">
+              <div className="grid grid-cols-12 ">
+                <div className="col-span-6">
+                  <h2 className="text-[50px] font-semibold text-btn dark:text-theme">
+                    <TrackVisibility once>
+                      {({ isVisible }) =>
+                        isVisible && (
+                          <>
+                            <CountUp delay={0} start={0} end={12} />+
+                          </>
+                        )
+                      }
+                    </TrackVisibility>
+                  </h2>
+                  <p className="text-text dark:text-white">
+                    Years of <br /> Experience
+                  </p>
+                </div>
+                <div className="col-span-6">
+                  <h2 className="text-[50px] font-semibold text-btn dark:text-theme">
+                    <TrackVisibility once>
+                      {({ isVisible }) =>
+                        isVisible && (
+                          <>
+                            <CountUp delay={0} start={0} end={350} />+
+                          </>
+                        )
+                      }
+                    </TrackVisibility>
+                  </h2>
+                  <p className="text-text dark:text-white">
+                    Project <br /> Completed
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-12 md:col-span-7">
+            <div className="rounded-xl bg-white p-[30px] dark:bg-card">
+              <div className="flex justify-center gap-[20px]">
+                <div className="overflow-hidden rounded-lg">
+                  <div>
+                    <img
+                      src="/assets/images/show/image-1.png"
+                      alt=""
+                      className="transition-all duration-300 hover:scale-[110%]"
+                    />
+                  </div>
+                </div>
+                <div className="overflow-hidden rounded-lg">
+                  <div>
+                    <img
+                      src="/assets/images/show/image-2.png"
+                      alt=""
+                      className="transition-all duration-300 hover:scale-[110%]"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mt-[30px]">
+                <p className="text-[14px] text-text dark:text-white">
+                  Showcase View
+                </p>
+                <Link href={"/projects"}>
+                  <h3 className="mt-[5px] inline-block text-[26px] tracking-[6px] text-btn dark:text-white">
+                    Projects
+                  </h3>
+                </Link>
+              </div>
+            </div>
+            <div className="mt-[30px] grid grid-cols-12 gap-[30px]">
+              <div className="col-span-6 rounded-xl bg-theme px-[30px] py-[25px]">
+                <p className="text-[14px] text-btn">Visit Our Blog</p>
+                <Link href={"/blog"}>
+                  <h3 className="mt-[5px] inline-block text-[26px] tracking-[6px] text-btn">
+                    Blog
+                  </h3>
+                </Link>
+              </div>
+              <div className="hero-pattern col-span-6 rounded-xl bg-white px-[30px]	py-[25px] dark:bg-card">
+                <p className="text-[14px] text-text dark:text-white">
+                  Let’s work
+                </p>
+                <Link href={"/contact"}>
+                  <h3 className="mt-[5px] inline-block text-[26px] tracking-[6px] text-btn dark:text-white">
+                    Contact
+                  </h3>
+                </Link>
+              </div>
+            </div>
+            <div className="mt-[30px] ">
+              <div
+                className="relative flex h-[185px] items-end rounded-xl bg-cover bg-local bg-center bg-no-repeat  px-[30px] py-[30px]"
+                style={{
+                  backgroundImage: `url(${"/assets/images/show/image-4.png"})`,
+                }}
+              >
+                <div>
+                  <p className="text-[14px] text-white">What Client Says?</p>
+                  <Link href={"/testimonial"}>
+                    <h3 className="mt-[5px] text-[26px] tracking-[6px] text-white">
+                      Testimonials
+                    </h3>
+                  </Link>
+                  <img
+                    className="absolute right-[30px] top-[50px] z-10"
+                    src="/assets/images/icon/shape.svg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ Master/src/app/projects/page.jsx
@@ -0,0 +1,191 @@
+"use client";
+import Link from "next/link";
+const page = () => {
+  return (
+    <section className=" mt-[60px] ">
+      {/* Project Intro */}
+      <div className="rounded-xl bg-white px-[20px] py-[40px] dark:bg-card md:p-[40px]">
+        <div>
+          <h1 className="text-[42px] leading-[52px] text-btn dark:text-white md:text-[62px] md:leading-[72px]">
+            Featured Projects<span className="text-theme">!</span>
+          </h1>
+          <p className="text-[14px] text-text">
+            Featured and popular products are the best-selling items for your
+            product, product range, or company. However, a featured product
+            could be a new product line or seasonal products instead.
+          </p>
+          <div className="mt-[60px]">
+            <div className="grid grid-cols-12 md:gap-[30px]">
+              <div className="col-span-12 mb-[40px] md:col-span-6 md:mb-0">
+                <div className="boxShadow group rounded-xl bg-white p-[20px] dark:bg-btn dark:shadow-none">
+                  <div className="overflow-hidden rounded-lg">
+                    <img
+                      src="/assets/images/show/image-5.png"
+                      alt=""
+                      className="transition-all duration-500 group-hover:scale-[110%]"
+                    />
+                  </div>
+                  <div className="mb-[10px] mt-[20px]">
+                    <p className="text-[16px] text-text">Mobile App Design</p>
+                    <Link href="/projects-details">
+                      <h2 className="mt-[14px] inline-block text-[30px] leading-[40px] text-btn transition-all duration-300 hover:text-theme dark:text-white dark:hover:text-theme">
+                        Mobile Application Landing Page Design
+                      </h2>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-12 mb-[40px] md:col-span-6 md:mb-0">
+                <div className="boxShadow group rounded-xl bg-white p-[20px] dark:bg-btn dark:shadow-none">
+                  <div className="overflow-hidden rounded-lg">
+                    <img
+                      src="/assets/images/show/image-6.png"
+                      alt=""
+                      className="transition-all duration-500 group-hover:scale-[110%]"
+                    />
+                  </div>
+                  <div className="mb-[10px] mt-[20px]">
+                    <p className="text-[16px] text-text">
+                      Web Design & Development
+                    </p>
+                    <Link href="/projects-details">
+                      <h2 className="mt-[14px] inline-block text-[30px] leading-[40px] text-btn transition-all duration-300 hover:text-theme dark:text-white dark:hover:text-theme">
+                        Website Design for Marketing Agency Startup
+                      </h2>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-12 mb-[40px] md:col-span-6 md:mb-0">
+                <div className="boxShadow group rounded-xl bg-white p-[20px] dark:bg-btn dark:shadow-none">
+                  <div className="overflow-hidden rounded-lg">
+                    <img
+                      src="/assets/images/show/image-7.png"
+                      alt=""
+                      className="transition-all duration-500 group-hover:scale-[110%]"
+                    />
+                  </div>
+                  <div className="mb-[10px] mt-[20px]">
+                    <p className="text-[16px] text-text">
+                      React Native Application
+                    </p>
+                    <Link href="/projects-details">
+                      <h2 className="mt-[14px] inline-block text-[30px] leading-[40px] text-btn transition-all duration-300 hover:text-theme dark:text-white dark:hover:text-theme">
+                        Travel App Design Creativity & React native Application
+                      </h2>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-12 mb-[40px] md:col-span-6 md:mb-0">
+                <div className="boxShadow group rounded-xl bg-white p-[20px] dark:bg-btn dark:shadow-none">
+                  <div className="overflow-hidden rounded-lg">
+                    <img
+                      src="/assets/images/show/image-8.png"
+                      alt=""
+                      className="transition-all duration-500 group-hover:scale-[110%]"
+                    />
+                  </div>
+                  <div className="mb-[10px] mt-[20px]">
+                    <p className="text-[16px] text-text">UX / UI Design </p>
+                    <Link href="/projects-details">
+                      <h2 className="mt-[14px] inline-block text-[30px] leading-[40px] text-btn transition-all duration-300 hover:text-theme dark:text-white dark:hover:text-theme">
+                        Project Management System with Admin Panel
+                      </h2>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-12 mb-[40px] md:col-span-6 md:mb-0">
+                <div className="boxShadow group rounded-xl bg-white p-[20px] dark:bg-btn dark:shadow-none">
+                  <div className="overflow-hidden rounded-lg">
+                    <img
+                      src="/assets/images/show/image-9.png"
+                      alt=""
+                      className="transition-all duration-500 group-hover:scale-[110%]"
+                    />
+                  </div>
+                  <div className="mb-[10px] mt-[20px]">
+                    <p className="text-[16px] text-text">
+                      UI / UX Design Mobile App{" "}
+                    </p>
+                    <Link href="/projects-details">
+                      <h2 className="mt-[14px] inline-block text-[30px] leading-[40px] text-btn transition-all duration-300 hover:text-theme dark:text-white dark:hover:text-theme">
+                        Native App Design Creativity with admin panel
+                      </h2>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-12 mb-[40px] md:col-span-6 md:mb-0">
+                <div className="boxShadow group rounded-xl bg-white p-[20px] dark:bg-btn dark:shadow-none">
+                  <div className="overflow-hidden rounded-lg">
+                    <img
+                      src="/assets/images/show/image-10.png"
+                      alt=""
+                      className="transition-all duration-500 group-hover:scale-[110%]"
+                    />
+                  </div>
+                  <div className="mb-[10px] mt-[20px]">
+                    <p className="text-[16px] text-text">UX / UI Design</p>
+                    <Link href="/projects-details">
+                      <h2 className="mt-[14px] inline-block text-[30px] leading-[40px] text-btn transition-all duration-300 hover:text-theme dark:text-white dark:hover:text-theme">
+                        Restaurant Mobile Application Figma Design
+                      </h2>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Pagination */}
+          <div className="pagination mt-6 flex justify-center md:mt-16">
+            <nav aria-label="Page navigation">
+              <ul className="inline-flex space-x-2">
+                <li>
+                  <button className="focus:shadow-outline flex h-10 w-10 items-center justify-center rounded-full bg-btn text-[#fff] transition-colors duration-150  hover:bg-theme">
+                    <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
+                      <path
+                        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
+                </li>
+                <li>
+                  <button className="focus:shadow-outline h-10 w-10 rounded-full  border-[#17b978] bg-theme text-btn transition-colors duration-150">
+                    1
+                  </button>
+                </li>
+                <li>
+                  <button className="focus:shadow-outline h-10 w-10 rounded-full bg-btn text-[#fff] transition-colors duration-150  hover:bg-theme">
+                    2
+                  </button>
+                </li>
+                <li>
+                  <button className="focus:shadow-outline h-10 w-10 rounded-full bg-btn text-[#fff] transition-colors duration-150  hover:bg-theme">
+                    3
+                  </button>
+                </li>
+                <li>
+                  <button className="focus:shadow-outline flex h-10 w-10 items-center justify-center rounded-full bg-btn text-[#fff] transition-colors duration-150  hover:bg-theme">
+                    <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
+                      <path
+                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default page;
